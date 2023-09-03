cc681e0180da58953e25cf4b1c8e462a73a5a84f
morbidbark <ryan.v.sampson@protonmail.com>
2023-09-03 00:16:32 -0400

    Create skeleton for subscription endpoint

--- src/lib.rs
+++ src/lib.rs
@@ -6,10 +6,22 @@ async fn health_check() -> impl Responder {
     HttpResponse::Ok()
 }
 
+#[derive(serde::Deserialize)]
+struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
+    HttpResponse::Ok().finish()
+}
+
 pub fn run(listener: TcpListener) -> std::io::Result<Server> {
-    Ok(
-        HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-            .listen(listener)?
-            .run(),
-    )
+    Ok(HttpServer::new(|| {
+        App::new()
+            .route("/subscriptions", web::post().to(subscribe))
+            .route("/health_check", web::get().to(health_check))
+    })
+    .listen(listener)?
+    .run())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -6,7 +6,7 @@ async fn health_check() {
     let client = reqwest::Client::new();
 
     let response = client
-        .get(format!("http://{}/health_check", addr))
+        .get(format!("{}/health_check", addr))
         .send()
         .await
         .expect("Failed to execute request");
@@ -15,10 +15,55 @@ async fn health_check() {
     assert_eq!(Some(0), response.content_length());
 }
 
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(format!("{}/subscriptions", addr))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request");
+
+    assert_eq!(200, response.status().as_u16());
+}
+
+#[tokio::test]
+async fn subscribe_returns_400_when_data_missing() {
+    let addr = spawn_app();
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=le%20guin", "missing name"),
+        ("email=ursula_le_guin%40gmail.com", "missing email"),
+        ("", "missing name and email"),
+    ];
+
+    for (invalid_body, message) in test_cases {
+        let response = client
+            .post(format!("{}/subscriptions", addr))
+            .header("Content-Type", "application/x-www-form-urlencode")
+            .body(invalid_body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "API did not fail as expected with request body: {}",
+            message,
+        );
+    }
+}
+
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind address");
     let addr = listener.local_addr().unwrap().to_string();
     let server = zero2prod::run(listener).expect("Failed to start server");
     let _ = tokio::spawn(server);
-    addr
+    format!("http://{}", addr)
 }
